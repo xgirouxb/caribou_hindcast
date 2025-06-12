21b45307e3836a492ff645630e7cf47afbad16e1
xgirouxb <xgirouxb@gmail.com>
2025-06-12 15:40:19 -0400

    Improve export_geotiffs() function
    Remove the internal use of deparse(substitute(layer)) by defining an output file name argument instead. This is a more standard approach and much more robust.

--- R/export_pipeline_outputs.R
+++ R/export_pipeline_outputs.R
@@ -1,24 +1,31 @@
 export_geotiffs <- function(
     layer,
-    output_dir,
+    output_filename,
     append_layer_name = FALSE,
     gdal_datatype
 ) {
   
   # Create output directory if it doesn't exist
-  if(!fs::dir_exists(output_dir)) { fs::dir_create(output_dir) }
-  
-  # Create an output file name
-  output_filename <- paste0(
-    output_dir,
-    # Extract name of input target
-    deparse(substitute(layer)),
-    # Append rast layer name (for layers with multiple years)
-    if (append_layer_name) {
-      paste0("_", names(layer))
-    },
-    ".tif"
-  )
+  if(!fs::dir_exists(fs::path_dir(output_filename))) { 
+    fs::dir_create(fs::path_dir(output_filename)) 
+  }
+
+  # Append rast layer name (for layers with multiple years)
+  if (append_layer_name) {
+    output_filename <- fs::path(
+      # Directory
+      fs::path_dir(output_filename),
+      # New output filename
+      paste0(
+        # Conserve name
+        fs::path_ext_remove(fs::path_file(output_filename)),  
+        # Append layer names
+        "_", names(layer),    
+        # Add original extension
+        ".", fs::path_ext(output_filename)
+      )
+    )
+  }    
   
   # Write raster to output folder
   terra::writeRaster(
